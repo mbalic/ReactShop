8cee6128cd455b552c400c8b8d74703e0e1a5ef4
Mario Balic <mario.balic1@gmail.com>
2021-01-14 23:30:36 +0100

    added mocked products

---
+++ frontend/src/products.js
@@ -0,0 +1,122 @@
+const products = [
+    {
+      "_id": 1,
+      "name": "Laptop 1",
+      "image": "images/laptop1.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 9,
+      "rating": 3.4,
+      "numReviews": 3
+    },
+    {
+      "_id": 2,
+      "name": "Laptop 2",
+      "image": "images/laptop2.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 0,
+      "rating": 3.4,
+      "numReviews": 8
+    },
+    {
+      "_id": 3,
+      "name": "Laptop 3",
+      "image": "images/laptop3.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 6,
+      "rating": 3.4,
+      "numReviews": 3
+    },
+    {
+      "_id": 4,
+      "name": "Laptop 4",
+      "image": "images/laptop4.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 8,
+      "rating": 3.4,
+      "numReviews": 7
+    },
+    {
+      "_id": 5,
+      "name": "Laptop 5",
+      "image": "images/laptop5.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 5,
+      "rating": 3.4,
+      "numReviews": 9
+    },
+    {
+      "_id": 6,
+      "name": "Laptop 6",
+      "image": "images/laptop6.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 9,
+      "rating": 3.4,
+      "numReviews": 0
+    },
+    {
+      "_id": 7,
+      "name": "Laptop 7",
+      "image": "images/laptop7.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 10,
+      "rating": 3.4,
+      "numReviews": 8
+    },
+    {
+      "_id": 8,
+      "name": "Laptop 8",
+      "image": "images/laptop8.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 3,
+      "rating": 3.4,
+      "numReviews": 9
+    },
+    {
+      "_id": 9,
+      "name": "Laptop 9",
+      "image": "images/laptop9.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 2,
+      "rating": 3.4,
+      "numReviews": 10
+    },
+    {
+      "_id": 10,
+      "name": "Laptop 10",
+      "image": "images/laptop10.png",
+      "description": "Laptop description text about how cool is this laptop and why you should buy it.",
+      "brand": "HP",
+      "category": "Electronics",
+      "price": 299.99,
+      "countInStock": 9,
+      "rating": 3.4,
+      "numReviews": 1
+    }
+  ]
